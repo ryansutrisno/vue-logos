f7b4bfe0459adc64f80b6f4efe0eb4c5a9acb798
Ryan Sutrisno <ryan.sutrisno@gmail.com>
2020-02-12 23:53:05 +0700

    commit project

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Logos/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Logos from './components/Logos.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Logos
   }
 }
 </script>
---
+++ src/components/Logos.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="logos-wrapper">
+        <h2 class="title-header">Find Best Logo</h2>
+        <div class="item-logos">
+            <img class="logos" alt="Adidas logo" src="../assets/adidas-logo.png">
+        </div>
+        <div class="item-logos">
+            <img class="logos" alt="Nike logo" src="../assets/nike-logo.png">
+        </div>
+        <div class="item-logos">
+            <img class="logos" alt="Puma logo" src="../assets/puma-logo.png">
+        </div>
+        <div class="item-logos">
+            <img class="logos" alt="Adidas logo" src="../assets/adidas-logo.png">
+        </div>
+        <div class="item-logos">
+            <img class="logos" alt="Nike logo" src="../assets/nike-logo.png">
+        </div>
+        <div class="item-logos">
+            <img class="logos" alt="Puma logo" src="../assets/puma-logo.png">
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Logo'
+}
+</script>
+
+<style scoped>
+.title-header {
+    margin-bottom: 80px;
+    text-align: left;
+}
+.logos-wrapper {
+    width: 62%;
+    overflow: hidden;
+    margin: auto;
+}
+.item-logos {
+    float: left;
+    margin-right: 70px;
+    margin-bottom: 70px;
+    width: 150px;
+    height: 150px;
+}
+.item-logos .logos {
+    width: 150px;
+    height: 150px;
+}
+</style>
